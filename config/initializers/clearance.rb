fad5bf7de53152eddec4bc4062f1baf8aa5762a6
Chineze Nwosu <chineze.nwosu@andela.com>
2016-09-01 14:26:34 +0100

    implement remember me feature, add columns to user migration

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,3 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = Figaro.env.password_reset_sender_email
+  config.cookie_expiration = lambda {|cookies| cookies[:remember_me] ? 1.years.from_now : nil }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
-  root "bookings#index"
   resources :bookings
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+
+  constraints Clearance::Constraints::SignedIn.new do
+    root 'bookings#new', as: :signed_in_root
+  end
+
+  constraints Clearance::Constraints::SignedOut.new do
+    root 'bookings#index'
+end
 end
