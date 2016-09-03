a16cf7dfa71052191df4ab049fb09a3197107fdc
Chineze Nwosu <chineze.nwosu@andela.com>
2016-09-03 16:30:35 +0100

    add has_secure_password to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def index
-      
+      @user = User.new
   end
 
   def new
@@ -12,23 +12,18 @@ class UsersController < ApplicationController
   end
 
   def create
-      @user = User.new
-      @user.first_name = params[:user][:first_name]
-      @user.last_name = params[:user][:last_name]
-      @user.email = params[:user][:email]
-      @user.password_digest = Auth.encrypt(params[:password])
-      binding.pry
-      if @user.save 
-        # take user to dashboard
-
+    @user = User.new(user_params)
+      if @user.save
+        render :json => {:success => true}
       else
-        render :new
+        errors = @user.errors.full_messages
+        render :json => {:success => false, :errors => errors}
       end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users
   resources :bookings
-  root to: "home#index"
+  root to: "users#index"
 end
