5ffc2e06e7b1cfe5ede448ebb304e349fdc9514a
Chineze Nwosu <chineze.nwosu@andela.com>
2016-09-09 12:01:28 +0100

    install responders gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   resources :users
   resources :bookings
   resources :sessions
+  resources :flights
 
   root 'users#index'
   get 'users/index', to: 'users#index', as: 'user_home'
   get 'home/index', to: 'home#index', as: 'home'
+  get 'schedule', to: 'users#schedule', as: 'schedule'
+  post 'search', to: 'flights#search'
   post 'login', to: 'sessions#create'
   post 'signup', to: 'users#create'
   get 'logout', to: 'sessions#destroy'
